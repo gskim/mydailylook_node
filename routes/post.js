2a1edd1b266f1142d47218a5349894f963c064c8
root <root@mydailylook2.cafe24.com>
2016-09-16 15:57:23 +0900

    multiparty 추가 적용

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,7 @@ var config = require('../config.json');
 var moment = require('moment');
 var version = config.appVersion;
 var multer = require('multer');
+var Promise = require('promise');
 var storage = multer.diskStorage({
     // 서버에 저장할 폴더
     destination: function (req, file, cb) {
@@ -25,12 +26,13 @@ var storage = multer.diskStorage({
       cb(null, file.uploadedFile.name + '.' + file.uploadedFile.ext);
     }
   });
-var upload = multer({ storage: storage }).fields([
-                                                  { name: 'file1', maxCount: 1 },
-                                                  { name: 'file2', maxCount: 1 },
-                                                  { name: 'file3', maxCount: 1 },
-                                                  { name: 'file4', maxCount: 1 }
-                                                ]);
+//var upload = multer({ storage: storage }).fields([
+//                                                  { name: 'file1', maxCount: 1 },
+//                                                  { name: 'file2', maxCount: 1 },
+//                                                  { name: 'file3', maxCount: 1 },
+//                                                  { name: 'file4', maxCount: 1 }
+//                                                ]);
+var upload = multer({storage : storage}).single('file1');
 
 var easyimg = require('easyimage');
 
@@ -79,36 +81,70 @@ router.get('/resizing' , function(req , res){
 );
   res.json('');
 });
-
-
+var multiparty = require('multiparty');
+var fs = require('fs');
+//router.post('/posting'   , function( req ,res ){
+//	var form = new multiparty.Form();
+//    
+//    // get field name & value
+//    form.on('field',function(name,value){
+//         console.log('normal field / name = '+name+' , value = '+value);
+//    });
+//    
+// // file upload handling
+//    form.on('part',function(part){
+//         var filename;
+//         var size;
+//         if (part.filename) {
+//               filename = part.filename;
+//               size = part.byteCount;
+//         }else{
+//               part.resume();
+//        
+//         }    
+//         console.log(part);
+//         console.log("Write Streaming file :"+filename);
+//         var writeStream = fs.createWriteStream('uploads/' + moment().format('YYYYMMDD') + '/'+filename);
+//         writeStream.filename = filename;
+//         part.pipe(writeStream);
+//
+//         part.on('data',function(chunk){
+//               console.log(filename+' read '+chunk.length + 'bytes');
+//         });
+//        
+//         part.on('end',function(){
+//               console.log(filename+' Part read complete');
+//               writeStream.end();
+//         });
+//    });
+//    
+//    form.on('close',function(){
+//    	res.json({
+//  		  code : 1
+//  	  });
+//   });
+//    form.parse(req);
+//	  
+//	});
 router.post('/posting'   , function( req ,res ){
   var accessToken = req.body.accessToken;
-  console.log(accessToken);
 	var content = req.body.contents;
 	var tag = req.body.tag;
 	var permission = req.body.permission;
 	var place_name = req.body.place_name;
 	var place_position = req.body.place_position;
-	console.log(req.files);
-	upload(req , res , function(err){
-		if (err){
-			console.log(err);
-			throw err;
-		}
-//		var data  = {
-//                email:email,
-//                password: password,
-//                regdate : now,
-//                login_time : now,
-//                login_type : loginType,
-//                email_token : email_token,
-//                nickname : deviceId
-//              };
-//		var query = connection.query(' INSERT INTO posts SET ? ' , data , function(err , result){
-//
-//		});
-	})
-
+	var promise = new Promise(function(resolved , rejected){
+		upload(req , res , function(err){
+			if (err) rejected(err);
+			
+			resolved(req);
+		})
+	}).catch(function(err){
+		console.log('err : ' + err);
+	}).then(function(req){
+		console.log(req.body.tag);
+		console.log(req.file);
+	});
   res.json({
 	  code : 1
   });
